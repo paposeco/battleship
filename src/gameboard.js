af558c482693d97d53584077267f96425c5b7d5c
paposeco <13892562+paposeco@users.noreply.github.com>
2021-08-11 16:26:05 +0200

    can place ships on a board

--- src/gameboard.js
+++ src/gameboard.js
@@ -48,10 +48,15 @@ const pickLocation = function (obj) {
 const placeShip = (function () {
   const ship = shipFleet[0];
   const shipLength = ship.length;
-  let randomLocation = pickLocation(newBoard.coordinates).split(",");
-  const row = Number(randomLocation[0]);
-  const column = Number(randomLocation[1]);
-  const whereToCheck = (function () {
+  // let randomLocation = pickLocation(newBoard.coordinates).split(",");
+  // const row = Number(randomLocation[0]);
+  // const column = Number(randomLocation[1]);
+
+  let randomLocation;
+  let row;
+  let column;
+
+  const whereToCheckRow = function (currentboard) {
     let array = [];
     for (let i = 1; i < shipLength; i++) {
       const shiftedColumn = column - i;
@@ -59,7 +64,10 @@ const placeShip = (function () {
         continue;
       }
       const location = row + "," + shiftedColumn;
-      array.push(location);
+      const statusOnLocation = currentboard.coordinates[location];
+      if (statusOnLocation === 0) {
+        array.push(shiftedColumn);
+      }
     }
     for (let j = 1; j < shipLength; j++) {
       const shiftedColumn = column + j;
@@ -67,10 +75,175 @@ const placeShip = (function () {
         continue;
       }
       const location = row + "," + shiftedColumn;
-      array.push(location);
+      const statusOnLocation = currentboard.coordinates[location];
+      if (statusOnLocation === 0) {
+        array.push(shiftedColumn);
+      }
     }
     return array;
-  })();
-  console.log(randomLocation);
-  console.log(whereToCheck);
+  };
+  const whereToCheckColumn = function (currentboard) {
+    let array = [];
+    for (let i = 1; i < shipLength; i++) {
+      const shiftedRow = row - i;
+      if (shiftedRow < 1) {
+        continue;
+      }
+      const location = shiftedRow + "," + column;
+      const statusOnLocation = currentboard.coordinates[location];
+      if (statusOnLocation === 0) {
+        array.push(shiftedRow);
+      }
+    }
+    for (let j = 1; j < shipLength; j++) {
+      const shiftedRow = row + j;
+      if (shiftedRow > 10) {
+        continue;
+      }
+      const location = shiftedRow + "," + column;
+      const statusOnLocation = currentboard.coordinates[location];
+      if (statusOnLocation === 0) {
+        array.push(shiftedRow);
+      }
+    }
+    return array;
+  };
+
+  const sumSequence = function (a, b) {
+    let sum = 0;
+    for (let i = 0; i <= b; i++) {
+      sum += a + i;
+    }
+    return sum;
+  };
+
+  const consecutiveNumbers = function (array, currentshiplength) {
+    for (let i = 0; i < array.length; i++) {
+      const beginningOfShip = array[i];
+      //the first position also counts
+      const endOfShip = beginningOfShip + currentshiplength - 1;
+      if (endOfShip > array[array.length]) {
+        break;
+      }
+      const consecutive = sumSequence(beginningOfShip, endOfShip);
+
+      const inarray = sumSequence(
+        beginningOfShip,
+        array[i + currentshiplength - 1]
+      );
+      if (consecutive === inarray) {
+        return beginningOfShip;
+      } else {
+        console.log("fails");
+        continue;
+      }
+    }
+  };
+
+  const compareNumbers = function (a, b) {
+    return a - b;
+  };
+
+  const checkForFit = function (currentship, currentboard) {
+    const selectedInitialLocation = row + "," + column;
+    const availableSpacesRow = whereToCheckRow(currentboard)
+      .concat(column)
+      .sort(compareNumbers);
+    const availableSpacesColumn = whereToCheckColumn(currentboard)
+      .concat(row)
+      .sort(compareNumbers);
+    // pick randomly row or column (not really random: if row is odd check column if even check row)
+
+    if (row % 2 === 1) {
+      if (availableSpacesRow.length < currentship.length) {
+        if (availableSpacesColumn.length < currentship.length) {
+          return false;
+        } else {
+          const startposition = consecutiveNumbers(
+            availableSpacesColumn,
+            currentship.length
+          );
+          return [row, startposition, "horizontal"];
+        }
+      } else {
+        const startposition = consecutiveNumbers(
+          availableSpacesRow,
+          currentship.length
+        );
+        return [startposition, column, "vertical"];
+      }
+    } else {
+      if (availableSpacesColumn.length < currentship.length) {
+        if (availableSpacesRow.length < currentship.length) {
+          return false;
+        } else {
+          const startposition = consecutiveNumbers(
+            availableSpacesRow,
+            currentship.length
+          );
+          return [startposition, column, "vertical"];
+        }
+      } else {
+        const startposition = consecutiveNumbers(
+          availableSpacesColumn,
+          currentship.length
+        );
+        return [row, startposition, "horizontal"];
+      }
+    }
+  };
+  const changeBoard = function (boardobj, currentship) {
+    randomLocation = pickLocation(newBoard.coordinates).split(",");
+    row = Number(randomLocation[0]);
+    column = Number(randomLocation[1]);
+    console.log(boardobj);
+    const selectInitialPlacement = checkForFit(currentship, boardobj);
+    console.log(selectInitialPlacement);
+    if (!selectInitialPlacement) {
+      return "ERROR ERROR";
+    }
+    const currentshiplength = currentship.length;
+    const boatOrientation = selectInitialPlacement[2];
+    let coordinatesToChange = [];
+    if (boatOrientation === "vertical") {
+      for (let i = 0; i < currentshiplength; i++) {
+        const newcoord = [
+          Number(selectInitialPlacement[0]) +
+            i +
+            "," +
+            selectInitialPlacement[1],
+        ];
+        coordinatesToChange.push(newcoord);
+      }
+    } else {
+      for (let j = 0; j < currentshiplength; j++) {
+        const newcoord = [
+          selectInitialPlacement[0] +
+            "," +
+            (Number(selectInitialPlacement[1]) + j),
+        ];
+        coordinatesToChange.push(newcoord);
+      }
+    }
+    for (let k = 0; k < coordinatesToChange.length; k++) {
+      boardobj.coordinates[coordinatesToChange[k]] = 1;
+    }
+    return "done";
+  };
+  return { changeBoard };
+  // changeBoard(newBoard, ship);
 })();
+
+async function setupboard() {
+  let newBoard2 = createGameboard("playerA");
+  const teste = await placeShip.changeBoard(newBoard2, shipFleet[0]);
+  const segundoteste = placeShip.changeBoard(newBoard2, shipFleet[1]);
+  console.log(newBoard2);
+}
+
+setupboard();
+
+// console.log("primeiro");
+// console.log("segundo");
+// placeShip.changeBoard(newBoard2, shipFleet[1]);
+// console.log(newBoard2.coordinates);
